7a9e79b051bf75faa6585a8b6a39c27de1687886
Emirhan <emirsa24@gmail.com>
2026-03-24 10:35:41 +0300

    docker compose eklendi

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:2424@host.docker.internal:5432/library_db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:2424@db:5432/library_db'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
